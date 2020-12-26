80bcd9f16357bc6c5cb7e24139205589aa80d451
Anish G <anish@masalabox.com>
2020-12-26 10:46:29 +0530

    code commit

---
+++ app/src/main/java/com/gthecoder/myapplication/Fragments/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.gthecoder.myapplication.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.gthecoder.myapplication.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).setSupportActionBar(main_toolbar)
+
+        val actionbar = (activity as AppCompatActivity).supportActionBar
+        actionbar!!.title="GWallpaper App"
+
+    }
+}
